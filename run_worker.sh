11db8123f2185eddd5cfecb69732b4b77bd30083
p3df4r <pedfar321@gmail.com>
2024-10-23 12:00:06 -0300

    dont download files

--- run_worker.sh
+++ run_worker.sh
@@ -7,6 +7,4 @@ dnf install wget -y
 wget https://github.com/ablab/spades/releases/download/v4.0.0/SPAdes-4.0.0-Linux.tar.gz
 tar -xvzf SPAdes-4.0.0-Linux.tar.gz
 alias spades="python3 SPAdes-4.0.0-Linux/bin/spades.py"
-wget https://github.com/tseemann/shovill/blob/master/test/R1.fq.gz
-wget https://github.com/tseemann/shovill/blob/master/test/R2.fq.gz
 dask worker tcp://$1:8786 
